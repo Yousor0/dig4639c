9e5b6a6e3a27c7c0a9e9269db7673c0c75ea073e
Andrew J <andjiang2004@gmail.com>
2025-02-04 18:31:54 -0500

    Tortise and Hare

---
+++ src/components/Content.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Default.css";
+
+function Content() {
+  return (
+    <div>
+      <p>
+        Once upon a time, in a lush green forest, a proud and swift hare loved
+        to boast about his speed. He often teased the slow-moving tortoise,
+        laughing at how sluggish he was. Tired of the hare’s constant bragging,
+        the tortoise calmly challenged him to a race. Amused, the hare accepted,
+        confident that he would win with ease.
+      </p>
+      <p>
+        When the race began, the hare dashed ahead, quickly leaving the tortoise
+        far behind. Certain of his victory, he decided to rest under a tree and
+        take a nap. Meanwhile, the tortoise kept moving steadily, never stopping
+        or slowing down. By the time the hare woke up, the tortoise was near the
+        finish line. The hare sprinted as fast as he could, but it was too
+        late—the tortoise had already won.
+      </p>
+    </div>
+  );
+}
+
+export default Content;
---
+++ src/components/Footer.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Footer() {
+  return <p>Moral of the Story: Slow and steady wins the race.</p>;
+}
+
+export default Footer;
